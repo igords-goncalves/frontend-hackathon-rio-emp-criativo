556216bbec71c7dc0037c8935e0098a90c54574a
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-25 16:07:38 -0300

    Criando rotas de navegacao

---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router"
+import { Home } from "../pages/Home"
+
+
+export const MainRoutes = () => {
+    <Routes>
+        <Route path="/" element={<Home />} />
+    </Routes>
+}
