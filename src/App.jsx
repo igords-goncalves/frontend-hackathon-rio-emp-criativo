4fd97d4215c2dcc281ce124ffc16074b5936e527
Igor Gonçalves <igordossantos38@gmail.com>
2023-03-26 14:24:02 -0300

    fix: concertando bugs nas dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./Pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FormLoginSuplier } from "./pages/FormLoginSuplier/FormLoginSuplier";
 import { FormRegisterSuplier } from "./pages/FormRegisterSuplier/FormRegisterSuplier";
+import TelaCircuito from "./Pages/TelaCircuito/TelaCircuito";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<TelaLogin/>} />
         <Route path="/loginAccess" element={<FormLoginSuplier />} />
         <Route path="/loginRegister" element={<FormRegisterSuplier />} />
+        <Route path="/circuito" element={<TelaCircuito />} />
       </Routes>
     </BrowserRouter>
   );
