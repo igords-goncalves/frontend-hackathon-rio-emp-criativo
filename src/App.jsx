0b27efbf0747805900aec7c87e7df781ba81cea5
Eduardo Barros <edu.barros101@gmail.com>
2023-03-26 14:36:34 -0300

    Fix da Page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import TelaLogin from "./Pages/TelaLogin/TelaLogin";
-import Home from "./Pages/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FormLoginSuplier } from "./pages/FormLoginSuplier/FormLoginSuplier";
-import { FormRegisterSuplier } from "./pages/FormRegisterSuplier/FormRegisterSuplier";
-import TelaCircuito from "./Pages/TelaCircuito/TelaCircuito";
+import React from 'react'
+import TelaLogin from './pages/TelaLogin/TelaLogin'
+import Home from './pages/Home/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { FormLoginSuplier } from './pages/FormLoginSuplier/FormLoginSuplier'
+import { FormRegisterSuplier } from './pages/FormRegisterSuplier/FormRegisterSuplier'
+import TelaCircuito from './pages/TelaCircuito/TelaCircuito'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<TelaLogin/>} />
-        <Route path="/loginAccess" element={<FormLoginSuplier />} />
-        <Route path="/loginRegister" element={<FormRegisterSuplier />} />
-        <Route path="/circuito" element={<TelaCircuito />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<TelaLogin />} />
+        <Route path='/loginAccess' element={<FormLoginSuplier />} />
+        <Route path='/loginRegister' element={<FormRegisterSuplier />} />
+        <Route path='/circuito' element={<TelaCircuito />} />
       </Routes>
     </BrowserRouter>
-  );
-
+  )
 }
 
-export default App;
+export default App
